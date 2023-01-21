6a4c9700b19849f14023f6429886f2d7edc16e1a
Adler Faulkner <aif28@cornell.edu>
2023-01-20 15:34:54 -1000

    fix: update openapi operation executor package

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,18 @@ export * from '@comake/skql-js-engine/dist/util/Types';
 export * from '@comake/openapi-operation-executor/dist/OpenApiClientConfiguration';
 export * from '@comake/openapi-operation-executor/dist/OpenApiSchemaConfiguration';
 
+// OpenAPI Types
+export * from './openapi-types/OpenApiArgTypes';
+export * from './openapi-types/OpenApiOperationNamespace';
+export * from './openapi-types/OpenApiOperationType';
+export * from './openapi-types/OpenApiSpecOptions';
+
 // Operation Executor
 export * from './operation-executor/OpenApiOperationExecutor';
 export * from './operation-executor/OperationExecutor';
 
+export * from './ApiOperationNamespace';
+export * from './ApiSpecOptions';
+export * from './BaseApiSpecOptions';
+export * from './OperationHandler';
 export * from './StandardSdk';
